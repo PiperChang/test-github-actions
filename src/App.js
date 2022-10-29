8d3364e10588ed6ca279d506a7ace77cbef677ab
PiperChang <goddhqj8000@ajou.ac.kr>
2022-10-29 13:27:29 +0900

    Init: CRA

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>
+          Hello, CICD test
+          </h1>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/App.test.js
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const heading = screen.getByRole("heading")
+  expect(heading).toBeInTheDocument()
+  expect(heading.textContent).toBe("Hello, CICD test")
+});
